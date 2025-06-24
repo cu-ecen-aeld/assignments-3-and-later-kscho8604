32db37a2b696c72fc9c190f54fa6936f2581d92f
Kyungsik Cho <kscho8604@gmail.com>
2025-06-24 10:26:21 +0900

    add aesdsocket code.

---
+++ server/aesdsocket.c
@@ -0,0 +1,213 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <syslog.h>
+#include <errno.h>
+#include <libgen.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define TMP_FILE "/var/tmp/aesdsocketdata"
+#define WBUF_SIZE 1024
+#define PORT 9000
+
+int mkdir_recursive(const char *path, mode_t mode)
+{
+        char tmp[PATH_MAX];
+        char *p = NULL;
+        struct stat sb;
+        size_t len;
+
+        len = strnlen(path, PATH_MAX);
+        if(len == 0 || len == PATH_MAX) {
+                return -1;
+        }
+        memcpy(tmp, path, len);
+        tmp[len] = '\0';
+
+        for(p = tmp + 1; *p; p++) {
+                if(*p == '/') {
+                        *p = '\0';
+
+                        if(stat(tmp, &sb) != 0) {
+                                if(mkdir(tmp, mode) < 0) {
+                                        return -1;
+                                }
+                        }else if(!S_ISDIR(sb.st_mode)) {
+                                errno = ENOTDIR;
+                                return -1;
+                        }
+                        *p = '/';
+                }
+        }
+
+        if (stat(tmp, &sb) != 0) {
+                if (mkdir(tmp, mode) < 0) {
+                        return -1;
+                }
+        } else if (!S_ISDIR(sb.st_mode)){
+                errno = ENOTDIR;
+                return -1;
+        }
+
+        return 0;
+}
+
+int write_file(const char *writefile, const char *writestr, int leng)
+{
+        FILE *fp;
+        int ret = 0;
+        char *path_copy = strdup(writefile);
+        char *path;
+        mode_t permissions = 0777;
+
+        if (path_copy == NULL) {
+                perror("strdup");
+                syslog(LOG_ERR, "Erro stdup error \n");
+                ret = 1;
+                goto exit;
+        }
+
+        path = dirname(path_copy);
+
+        if(mkdir_recursive(path, permissions) == -1){
+                syslog(LOG_ERR, "Error make folder %s: %s\n", path, strerror(errno));
+                ret = 1;
+                goto exit;
+        }
+
+        fp = fopen(writefile, "a");
+        if(fp == NULL) {
+                syslog(LOG_ERR, "Error opening file %s: %s\n", writefile, strerror(errno));
+                ret = 1;
+                goto exit;
+        } else {
+                syslog(LOG_DEBUG, "Writing %s to %s\n", writestr, writefile);
+                fwrite((const void*)writestr, sizeof(char), leng, fp);
+                fclose(fp);
+        }
+
+exit:
+        free(path_copy);
+        return ret;
+}
+
+int fileLength(const char *readfile)
+{
+    FILE* fp;
+    
+    if ((fp = fopen(readfile, "r")) == NULL)
+    {
+        perror("read file"); 
+        return -1;
+    }
+    
+    //Find the length
+    fseek(fp, 0, SEEK_END);
+    int length = ftell(fp);
+    fclose(fp);
+    return length;
+}
+
+int readfile(const char *readfile, char *readdata, int leng)
+{
+    FILE *fp;
+
+    fp = fopen(readfile, "r");
+    if(fp == NULL) {
+        syslog(LOG_ERR, "Error opening file %s: %s\n", readfile, strerror(errno));
+        return -1;
+    } 
+            
+    for(int i = 0; i < leng; i++) {
+        readdata[i] = fgetc(fp);
+    }
+    fclose(fp);
+     
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    int sockfd, new_sockfd;
+    int domain = PF_INET;
+    int type = SOCK_STREAM;
+    int protocol = 0;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    int opt = 1;
+    char buffer[WBUF_SIZE] = {0};
+    char *readbuffer;
+
+	openlog(NULL, 0, LOG_USER);
+
+    sockfd = socket(domain, type, protocol);
+    if(sockfd < 0) {
+        perror("socket creation failed");
+        return -1; 
+    }
+
+
+    if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt failed");
+        return -1;
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if(bind(sockfd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind faild");
+        return -1;
+    }
+
+    if(listen(sockfd, 3) < 0) {
+        perror("listen");
+        return -1;
+    }
+
+    while(1) {
+        int leng;
+
+        if((new_sockfd = accept(sockfd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("accept");
+            return -1;
+        }
+
+        printf("Accepted connection from %s\n", inet_ntoa(address.sin_addr));
+
+        do {
+            leng = recv(new_sockfd, buffer, WBUF_SIZE, 0);
+
+            if(write_file(TMP_FILE, buffer, leng)) {
+                perror("write file");
+                return -1;
+            }
+        } while (leng == WBUF_SIZE);
+
+        leng = fileLength(TMP_FILE);
+
+        readbuffer = (char*)malloc(leng);  
+        if(readbuffer == NULL) {
+            perror("malloc readbuffer");
+            return -1;
+        }
+
+        if(readfile(TMP_FILE, readbuffer, leng)) {
+            perror("read file");
+            return -1;
+        }
+
+        leng = send(new_sockfd, readbuffer, leng, 0);
+        free(readbuffer); 
+    }
+ 
+	closelog();
+    
+	return 0;
+}
